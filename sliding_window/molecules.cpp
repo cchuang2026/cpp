597442ef8d7e5a2347d3205f4b3a5fb21afe443e
huang chengcheng <cchuang2026@gmail.com>
2026-07-01 17:18:30 +0800

    workupdate

---
+++ sliding_window/molecules.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World"<<endl;
+    int a = 10;
+    cout<<"The value of a is: "<<a<<endl;
+    a+= 5;
+    cout<<"The new value of a is: "<<a<<endl;
+    cout<<"This is a C++14 project"<<endl;
+}
