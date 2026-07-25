a6216c26977d591024e7723bbab1902263ad8d04
huang chengcheng <cchuang2026@gmail.com>
2026-07-25 11:20:42 +0800

    update

--- sliding_window/mirrors/mirrors.cpp
+++ sliding_window/mirrors/mirrors.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<climits>
 using namespace std;
 typedef long long ll;
 int main(){
@@ -19,23 +20,27 @@ int main(){
 
     ll ans=LLONG_MIN;
     for(int s=0;s<=n;s++){
-        ll left_sum,right_sum;
-        left_sum=prefix[s];
-        right_sum=prefix[n]-prefix[s];
+        ll left_sum=prefix[s];
+        ll right_sum=prefix[n]-prefix[s];
 
         if(left_sum<=right_sum){
             ll target=2*left_sum+1;
-            auto it=upper_bound(prefix.begin(),prefix.end(),target);
-            ll k=(long long)(it-prefix.begin())-1;
-            ans=max(ans,k);
-        }else if(left_sum>right_sum){
-            ll target=prefix[s]-right_sum;
 
-            auto it=lower_bound(prefix.begin(),prefix.begin()+s+1,target);
-            ll k=(long long)(it-prefix.begin());
-            ans=max(ans,n-k);
+            auto ub=upper_bound(prefix.begin(),prefix.end(),target);
+            ll cur_ans=(long long)(ub-prefix.begin())-1;
+            ans=max(cur_ans,ans);
+        }else if(left_sum>right_sum){
+
+            ll target=prefix[s]-right_sum-1;
+            auto it=lower_bound(prefix.begin(),prefix.begin()+s,target);
+
+            ll cur_ans=(long long)(prefix.end()-it)-1;
+
+            ans=max(ans,cur_ans);
+
         }
     }
 
     cout<<ans<<endl;
+    return 0;
 }
