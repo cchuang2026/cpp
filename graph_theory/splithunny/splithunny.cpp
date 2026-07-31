f7f62f975c1277f0e8d92b2ec95a3c6c96ca3ef4
huang chengcheng <cchuang2026@gmail.com>
2026-07-31 23:11:21 +0800

    dfs solve splithunny

--- graph_theory/splithunny/splithunny.cpp
+++ graph_theory/splithunny/splithunny.cpp
@@ -5,33 +5,10 @@
 #include<queue>
 using namespace std;
 //flood filling
-    int r, c;
-    char grids[105][105];
-    bool visited[105][105];
-    int dir[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-int main() {
-
-    cin >> r >> c;
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-        cin >> grids[i][j];
-        }
-    }
-
-    int pools = 0;
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            if (grids[i][j] == 'H' && !visited[i][j]) {
-                pools++;
-                bfs(i, j);
-            }
-        }
-    }
-
-    cout << "Oh, bother. There are " << pools << " pools of hunny.\n";
-    return 0;
-}
+int r, c;
+string grids[105];
+bool visited[105][105];
+int dir[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
 void bfs(int starti, int startj) {
     queue<pair<int, int>> q;
@@ -56,3 +33,40 @@ void bfs(int starti, int startj) {
         }
     }
 }
+
+void dfs(int starti, int startj){
+    visited[starti][startj] = true;
+
+    for (int k = 0; k < 4; ++k) {
+        int nexti = starti + dir[k][0];
+        int nextj = startj + dir[k][1];
+
+        if (nexti < 0 || nexti >= r || nextj < 0 || nextj >= c) continue;
+        if (grids[nexti][nextj] != 'H' || visited[nexti][nextj]) continue;
+        dfs(nexti,nextj);
+    }
+}
+
+int main() {
+    freopen("sample.in","r",stdin);
+
+    cin >> r >> c;
+    for (int i = 0; i < r; i++) {
+        cin >> grids[i];
+    }
+
+    int pools = 0;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            if (grids[i][j] == 'H' && !visited[i][j]) {
+                pools++;
+                dfs(i, j);
+            }
+        }
+    }
+
+    cout << "Oh, bother. There are " << pools << " pools of hunny.\n";
+    return 0;
+}
+
+
