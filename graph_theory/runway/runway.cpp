a28d0054e21d6a6895ab08a202b7a547b3e92513
huang chengcheng <cchuang2026@gmail.com>
2026-08-03 17:42:57 +0800

    runway done

--- graph_theory/runway/runway.cpp
+++ graph_theory/runway/runway.cpp
@@ -4,31 +4,58 @@
 using namespace std;
 
 const int INF = 1e9;
+int n, e, k;
+vector<vector<pair<int,int>>> adjlist;
 
-bool can(int x, vector<vector<pair<int,int>>>& adjlist, int n, int k) {
-    vector<int> cost(n+1);
+bool check(int x) {
+    vector<int> cost(n+1,INF);
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    
+    cost[1]=0;
+    pq.push({0,1}); //weight , node
+
+    while(!pq.empty()){
+        int cw=pq.top().first;
+
+        int cur_node=pq.top().second;
+        pq.pop();
+
+        if(cw>cost[cur_node]) continue;
+
+        for(auto[nw,next_node]:adjlist[cur_node]){
+            int next_weight=(nw>x)?1:0;
+
+            if(cost[next_node]>cost[cur_node]+next_weight){
+                cost[next_node]=cost[cur_node]+next_weight;
+
+                pq.push({cost[next_node],next_node});
+            }
+        }
+    }
+    return cost[n] <= k - 1;
 }
+
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int n, e, k;
+    // freopen("sample.in","r",stdin);
     cin >> n >> e >> k;
+    adjlist.resize(n+5);
     
-    vector<vector<pair<int,int>>> adjlist(n + 1);
     vector<int> allD;
     
     for (int i = 0; i < e; i++) {
         int a, b, d;
         cin >> a >> b >> d;
-        adjlist[a].emplace_back(d, b);   // {weight, node}
-        adjlist[b].emplace_back(d, a);
+        adjlist[a].push_back({d, b});   // {weight, node}
+        adjlist[b].push_back({d,a});
         allD.push_back(d);
     }
     
    //bfs to see if can output -1
+
    vector<int> dist(n+1,INF);//dist from 1
     vector<bool> visited(n+1,false);
     visited[1]=true;
@@ -46,6 +73,7 @@ int main() {
 
             visited[next_node]=true;
             dist[next_node]=dist[cur_node]+1;
+            q.push(next_node);
         }
 
     }
@@ -55,6 +83,7 @@ int main() {
         return 0;
     }
 
+    sort(allD.begin(),allD.end());
     int low=0;
     int high=(int)allD.size()-1;
     int ans=high;
@@ -63,6 +92,14 @@ int main() {
         int mid=(low+high)/2;
         int x=allD[mid];
 
-
+    if (check(x)) {
+        ans = x;
+        high = mid - 1;   // try smaller
+    } else {
+        low = mid + 1;
     }
+    }
+
+    cout<<ans<<endl;
+    return 0;
 }
